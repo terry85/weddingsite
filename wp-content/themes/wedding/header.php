8904ae24dfb0197383d4af1319ca2e8a7c6af991
Terry <terrykostenko1985@gmail.com>
2018-04-16 23:01:50 +0200

    responsive design

--- wp-content/themes/wedding/header.php
+++ wp-content/themes/wedding/header.php
@@ -26,7 +26,8 @@
 
     <div id="page" class="site">
 	<header id="masthead" class="site-header">
-		<nav>
+		<nav id="site-navigation" class="main-navigation">
+                    <button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'wedding' ); ?></button>
 			<?php
 			wp_nav_menu( array(
 				'theme_location' => 'menu-1',
